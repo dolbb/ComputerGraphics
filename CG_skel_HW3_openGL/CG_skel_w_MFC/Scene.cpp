101395bb0f004963b59fd7429ec4310d0f61e478
MR. D <dolbb@campus.technion.ac.il>
2018-01-08 20:03:44 +0200

    openGL demo setup

--- CG_skel_HW3_openGL/CG_skel_w_MFC/Scene.cpp
+++ CG_skel_HW3_openGL/CG_skel_w_MFC/Scene.cpp
@@ -2,6 +2,8 @@
 #include "Scene.h"
 #include "MeshModel.h"
 #include <string>
+#include "InitShader.h"
+#include "GL\freeglut.h"
 
 using namespace std;
 void Scene::loadOBJModel(string fileName)
@@ -20,6 +22,41 @@ void Scene::draw()
 
 void Scene::drawDemo()
 {
-	m_renderer->SetDemoBuffer();
-	m_renderer->SwapBuffers();
+	const int pnum = 3;
+	static const GLfloat points[pnum][4] = {
+		{-0.1, -0.1f, 0.0f,1.0f},
+		{0.1f, -0.1f, 0.0f,1.0f},
+		{0.0f,  0.1f, 0.0f,1.0f}
+	};
+
+	GLuint program = InitShader( "minimal_vshader.glsl", 
+					  "minimal_fshader.glsl" );
+
+	glUseProgram(program);
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+
+	GLuint buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points),
+		points, GL_STATIC_DRAW);
+
+
+
+	
+	GLuint loc = glGetAttribLocation(program, "vPosition");
+	glEnableVertexAttribArray(loc);
+	glVertexAttribPointer(loc, 4, GL_FLOAT, GL_FALSE, 0, 0);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glDrawArrays(GL_LINE_LOOP, 0, pnum);
+	glFlush();
+	glutSwapBuffers();
+	
+
 }
--- CG_skel_HW3_openGL/CG_skel_w_MFC/vshader.glsl
+++ CG_skel_HW3_openGL/CG_skel_w_MFC/vshader.glsl
@@ -1,16 +1,11 @@
 #version 150
 
 
-in  vec2 vTexCoord;
-in  vec2 vPosition;
+in  vec4 vPosition;
 
 
-out vec2 texCoord;
 
 void main()
 {
-    gl_Position.xy = vPosition;
-    gl_Position.z=0;
-    gl_Position.w=1;
-    texCoord = vTexCoord;
+    gl_Position = vPosition;
 }
