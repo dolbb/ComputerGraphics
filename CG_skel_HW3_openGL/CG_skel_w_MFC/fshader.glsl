49cdf392851402bcd08164719f453a2375c3c7cc
MR. D <dolbb@campus.technion.ac.il>
2018-01-08 20:08:24 +0200

    repair demo
    shaders are different than previous

--- CG_skel_HW3_openGL/CG_skel_w_MFC/fshader.glsl
+++ CG_skel_HW3_openGL/CG_skel_w_MFC/fshader.glsl
@@ -1,10 +1,15 @@
 
 #version 150
 
+in  vec2 texCoord;
+
 out vec4 fColor;
 
+uniform sampler2D texture;
 
 void main() 
 { 
-   fColor = vec4(0,1,1,1);
+   fColor = textureLod( texture, texCoord, 0 );
+   //fColor = vec4(0,0,1,1);
 } 
+
--- CG_skel_HW3_openGL/CG_skel_w_MFC/vshader.glsl
+++ CG_skel_HW3_openGL/CG_skel_w_MFC/vshader.glsl
@@ -1,11 +1,16 @@
 #version 150
 
 
-in  vec4 vPosition;
+in  vec2 vTexCoord;
+in  vec2 vPosition;
 
 
+out vec2 texCoord;
 
 void main()
 {
-    gl_Position = vPosition;
+    gl_Position.xy = vPosition;
+    gl_Position.z=0;
+    gl_Position.w=1;
+    texCoord = vTexCoord;
 }
