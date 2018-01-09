209bdaddc77f65ecda2f4ecf6349d890d763da81
MR. D <dolbb@campus.technion.ac.il>
2018-01-09 20:46:33 +0200

    vec and mat bugs fixed

--- CG_skel_HW3_openGL/CG_skel_w_MFC/mat.h
+++ CG_skel_HW3_openGL/CG_skel_w_MFC/mat.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "vec.h"
 
+#define Error( str ) do { std::cerr << "[" __FILE__ ":" << __LINE__ << "] " \
+				    << str << std::endl; } while(0)
 
 //----------------------------------------------------------------------------
 //
@@ -22,15 +24,16 @@ class mat2 {
     mat2( const vec2& a, const vec2& b )
 	{ _m[0] = a;  _m[1] = b;  }
 
-	/*BUG*/
-    mat2( GLfloat m00, GLfloat m10, GLfloat m01, GLfloat m11 )
-	{ _m[0] = vec2( 0, 0 ); _m[1] = vec2( 0, 0 ); }
+    mat2( GLfloat m00, GLfloat m01, GLfloat m10, GLfloat m11 ){
+		_m[0] = vec2( m00, m01 );
+		_m[1] = vec2( m10, m11 ); 
+	}
 
     mat2( const mat2& m ) {
-	if ( *this != m ) {
-	    _m[0] = m._m[0];
-	    _m[1] = m._m[1];
-	} 
+		if ( *this != m ) {
+			_m[0] = m._m[0];
+			_m[1] = m._m[1];
+		} 
     }
 
     //
@@ -48,27 +51,26 @@ class mat2 {
 	{ return mat2( _m[0]+m[0], _m[1]+m[1] ); }
 
 	
-    mat2 operator - ( const mat2& m ) const
-	{ return mat2( 0, 0 ); } /*BUG*/
+    mat2 operator - ( const mat2& m ) const{
+		return mat2(_m[0] - m[0], _m[1] - m[1]); 
+	}
 
     mat2 operator * ( const GLfloat s ) const 
 	{ return mat2( s*_m[0], s*_m[1] ); }
 
-    mat2 operator / ( const GLfloat s ) const {
-	
-	GLfloat r = GLfloat(1.0) / s;
-	return *this * r;
+    mat2 operator / ( const GLfloat s ) const {	
+		GLfloat r = GLfloat(1.0) / s;
+		return *this * r;
     }
 
     friend mat2 operator * ( const GLfloat s, const mat2& m )
 	{ return m * s; }
 	
     mat2 operator * ( const mat2& m ) const {
-	mat2  a( 0.0 );
-
-	/*BUG*/
-
-	return a;
+		return mat2(_m[0][0] * m[0][0] + _m[0][1] * m[1][0],	/*element m00*/
+					_m[0][0] * m[0][1] + _m[0][1] * m[1][1],	/*element m01*/
+					_m[1][0] * m[0][0] + _m[1][1] * m[1][0],	/*element m10*/
+					_m[1][0] * m[0][1] + _m[1][1] * m[1][1]);	/*element m11*/
     }
 
     //
@@ -81,7 +83,7 @@ class mat2 {
     }
 
     mat2& operator -= ( const mat2& m ) {
-	_m[0] -= 0;  _m[1] -= 0;  /*BUG*/
+	_m[0] -= m[0];  _m[1] -= m[1];  
 	return *this;
     }
 
@@ -91,11 +93,8 @@ class mat2 {
     }
 
     mat2& operator *= ( const mat2& m ) {
-	mat2  a( 0.0 );
-
-	/*BUG*/
-
-	return *this = a;
+		mat2  a(*this * m);
+		return *this = a;
     }
     
     mat2& operator /= ( const GLfloat s ) {
@@ -171,9 +170,9 @@ class mat3 {
     mat3( const vec3& a, const vec3& b, const vec3& c )
 	{ _m[0] = a;  _m[1] = b;  _m[2] = c;  }
 
-    mat3( GLfloat m00, GLfloat m10, GLfloat m20,
-	  GLfloat m01, GLfloat m11, GLfloat m21,
-	  GLfloat m02, GLfloat m12, GLfloat m22 ) 
+    mat3(	GLfloat m00, GLfloat m01, GLfloat m02,
+			GLfloat m10, GLfloat m11, GLfloat m12,
+			GLfloat m20, GLfloat m21, GLfloat m22 ) 
 	{
 	    _m[0] = vec3( m00, m01, m02 );
 	    _m[1] = vec3( m10, m11, m12 );
@@ -277,11 +276,15 @@ class mat3 {
     //  --- Matrix / Vector operators ---
     //
 
-    vec3 operator * ( const vec3& v ) const {  // m * v
-	return vec3( 0,
-		     0, /*BUG*/
-		     0 );
-    }
+	vec3 operator * (const vec3& v) const {  // m * v
+		return vec3(dot(_m[0], v),
+			dot(_m[1], v),
+			dot(_m[2], v));
+	}
+
+	friend vec3 operator * (const vec3& v, const mat3& m){  // v * m
+		return m * v;
+	}
 	
     //
     //  --- Insertion and Extraction Operators ---
@@ -295,33 +298,39 @@ class mat3 {
     }
 
     friend std::istream& operator >> ( std::istream& is, mat3& m )
-	{ return is >> m._m[0] >> m._m[1] >> m._m[2] ; }
+	{
+		return is >> m._m[0] >> m._m[1] >> m._m[2] ; 
+	}
 
     //
     //  --- Conversion Operators ---
     //
 
     operator const GLfloat* () const
-	{ return static_cast<const GLfloat*>( &_m[0].x ); }
+	{
+		return static_cast<const GLfloat*>( &_m[0].x ); 
+	}
 
     operator GLfloat* ()
-	{ return static_cast<GLfloat*>( &_m[0].x ); }
+	{
+		return static_cast<GLfloat*>( &_m[0].x ); 
+	}
 };
 
 //
 //  --- Non-class mat3 Methods ---
 //
 
-inline
-mat3 matrixCompMult( const mat3& A, const mat3& B ) {
+inline mat3 matrixCompMult( const mat3& A, const mat3& B ) {
     return mat3( A[0][0]*B[0][0], A[0][1]*B[0][1], A[0][2]*B[0][2],
 		 A[1][0]*B[1][0], A[1][1]*B[1][1], A[1][2]*B[1][2],
 		 A[2][0]*B[2][0], A[2][1]*B[2][1], A[2][2]*B[2][2] );
 }
 
-inline
-mat3 transpose( const mat3& A ) {
-    return mat3( 0,0,0,0,0,0,0,0,0); /*BUG*/
+inline mat3 transpose( const mat3& A ) {
+    return mat3(A[0][0], A[1][0], A[2][0], 
+				A[0][1], A[1][1], A[2][1], 
+				A[0][2], A[1][2], A[2][2]); 
 }
 
 //----------------------------------------------------------------------------
@@ -344,10 +353,10 @@ class mat4 {
     mat4( const vec4& a, const vec4& b, const vec4& c, const vec4& d )
 	{ _m[0] = a;  _m[1] = b;  _m[2] = c;  _m[3] = d; }
 
-    mat4( GLfloat m00, GLfloat m10, GLfloat m20, GLfloat m30,
-	  GLfloat m01, GLfloat m11, GLfloat m21, GLfloat m31,
-	  GLfloat m02, GLfloat m12, GLfloat m22, GLfloat m32,
-	  GLfloat m03, GLfloat m13, GLfloat m23, GLfloat m33 )
+	mat4(GLfloat m00, GLfloat m01, GLfloat m02, GLfloat m03,
+			GLfloat m10, GLfloat m11, GLfloat m12, GLfloat m13,
+			GLfloat m20, GLfloat m21, GLfloat m22, GLfloat m23,
+			GLfloat m30, GLfloat m31, GLfloat m32, GLfloat m33 )
 	{
 	    _m[0] = vec4( m00, m01, m02, m03 );
 	    _m[1] = vec4( m10, m11, m12, m13 );
@@ -363,6 +372,16 @@ class mat4 {
 		_m[2] = m._m[2];
 		_m[3] = m._m[3];
 	    } 
+	}
+	mat4(const mat3& m)
+	{
+		_m[0] = m[0];
+		_m[0][3] = 0;
+		_m[1] = m[1];
+		_m[1][3] = 0;
+		_m[2] = m[2];
+		_m[2][3] = 0;
+		_m[3] = vec4(0,0,0,1);
 	}
 
     //
@@ -386,10 +405,8 @@ class mat4 {
 	{ return mat4( s*_m[0], s*_m[1], s*_m[2], s*_m[3] ); }
 
     mat4 operator / ( const GLfloat s ) const {
-
-	
-	GLfloat r = GLfloat(1.0) / s;
-	return *this * r;
+		GLfloat r = GLfloat(1.0) / s;
+		return *this * r;
     }
 
     friend mat4 operator * ( const GLfloat s, const mat4& m )
@@ -514,22 +531,20 @@ mat4 transpose( const mat4& A ) {
 //
 //////////////////////////////////////////////////////////////////////////////
 
-#define Error( str ) do { std::cerr << "[" __FILE__ ":" << __LINE__ << "] " \
-				    << str << std::endl; } while(0)
-
-inline
+/* following function was given to us and was changed to a mat4 operator.
 vec4 mvmult( const mat4& a, const vec4& b )
 {
     Error( "replace with vector matrix multiplcation operator" );
 
     vec4 c;
     int i, j;
-    for(i=0; i<4; i++) {
-	c[i] =0.0;
-	for(j=0;j<4;j++) c[i]+=a[i][j]*b[j];
+	for (i = 0 ; i<4 ; ++i) {
+		c[i] = 0.0;
+		for(j=0;j<4;j++) 
+			c[i]+=a[i][j]*b[j];
     }
     return c;
-}
+}*/
 
 //----------------------------------------------------------------------------
 //
@@ -537,17 +552,58 @@ vec4 mvmult( const mat4& a, const vec4& b )
 //
 
 inline
-mat4 RotateX( const GLfloat theta )
+mat4 RotateX(const GLfloat theta)
 {
-    GLfloat angle = (M_PI/180.0) * theta;
+	GLfloat angle = (M_PI / 180.0) * theta;
 
-    mat4 c;
-    c[2][2] = c[1][1] = cos(angle);
-    c[2][1] = sin(angle);
-    c[1][2] = -c[2][1];
-    return c;
+	mat4 c;
+	c[2][2] = c[1][1] = cos(angle);
+	c[2][1] = sin(angle);
+	c[1][2] = -c[2][1];
+	return c;
 }
 
+inline
+mat4 RotateY(const GLfloat theta)
+{
+	GLfloat angle = (M_PI / 180.0) * theta;
+
+	mat4 c;
+	c[2][2] = c[0][0] = cos(angle);
+	c[2][0] = -sin(angle);
+	c[0][2] = -c[2][0];
+	return c;
+}
+
+inline
+mat4 RotateZ(const GLfloat theta)
+{
+	GLfloat angle = (M_PI / 180.0) * theta;
+
+	mat4 c;
+	c[0][0] = c[1][1] = cos(angle);
+	c[1][0] = sin(angle);
+	c[0][1] = -c[1][0];
+	return c;
+}
+
+inline
+mat4 RotateVec(vec3 p1, vec3 p2, GLfloat theta)
+{
+	vec3 v(p2[X] - p1[X], p2[Y] - p1[Y], p2[Z] - p1[Z]);
+	vec3 u = normalize(v); // u = (a,b,c)
+	GLfloat a = u[X], b = u[Y], c = u[Z];
+	GLfloat radAngle = (M_PI / 180.0) * theta;
+	GLfloat mCos = cos(radAngle);
+	GLfloat mSin = sin(radAngle);
+
+	//create rotaion mat over the given vector from p1 to p2.
+	mat4 r(	mCos+a*a*(1-mCos),		a*b*(1-mCos)-c*mSin,	a*c*(1-mCos)+b*mSin,  0,
+			a*b*(1-mCos)+c*mSin,	mCos + b*b*(1 - mCos),	b*c*(1-mCos)-a*mSin,  0,
+			a*c*(1-mCos)+b*mSin,	b*c*(1-mCos)+a*mSin,	mCos + c*c*(1 - mCos),0,
+			0,						0,						0,					  1);
+	return r;
+}
 
 //----------------------------------------------------------------------------
 //
@@ -558,9 +614,9 @@ inline
 mat4 Translate( const GLfloat x, const GLfloat y, const GLfloat z )
 {
     mat4 c;
-    c[0][0] = x;
-    c[0][0] = y;  /*BUG*/
-    c[0][0] = z;
+    c[0][3] = x;
+    c[1][3] = y;
+    c[2][3] = z;
     return c;
 }
 
@@ -586,8 +642,8 @@ mat4 Scale( const GLfloat x, const GLfloat y, const GLfloat z )
 {
     mat4 c;
     c[0][0] = x;
-    c[0][0] = y; /*BUG*/
-    c[0][0] = z;
+    c[1][1] = y;
+    c[2][2] = z;
     return c;
 }
 
